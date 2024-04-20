9856b6fd8eeaf081e3e27c06f32c27388d86d594
PawelRoz1 <112411462+PawelRoz1@users.noreply.github.com>
2024-04-20 15:48:50 -0500

    added functionality and aesthetic changes

--- reminders.js
+++ reminders.js
@@ -1,4 +1,7 @@
+var reminderTime = '8:30';
+
 function startTime() {
+    setReminder();
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
@@ -19,3 +22,32 @@ function militaryToAMPM(i) {
     return i;
 }
 
+function addAlarm() {
+    let h = document.getElementById('hour').value;
+    let m = document.getElementById('minute').value;
+    var ampm = 'am';
+    if (h < 0 || h > 23 || h === '') {
+        h = 8;
+    }
+    if (m < 0 || m > 59 || m === '') {
+        m = 30;
+    }
+    if (m < 10) 
+        m = '0' + m;
+    if (h > 11) { ampm = 'pm'};
+    h = militaryToAMPM(h);
+    
+    reminderTime = h + ':' + m + ' ' + ampm;
+    localStorage.setItem('reminderTime', reminderTime);
+    window.location.href = 'reminders.html';
+    //alert(reminderTime);    
+}
+
+
+function setReminder() {
+    reminderTime = localStorage.getItem('reminderTime');
+    if (reminderTime === null) 
+        reminderTime = '8:15';
+    //alert(reminderTime);
+    document.getElementById('alarm1').innerHTML = reminderTime;
+}
